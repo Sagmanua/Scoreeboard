094ffa8cd0248cfaa1b0a39da8c85e5e871bb86a
= <163444640+Sagmanua@users.noreply.github.com>
2026-03-31 19:00:12 +0200

    1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<div class="container">
+    <button @click="menos">-</button>
+    <span>{{ dia }}</span>
+    <button @click="plus">+</button>
+    <p v-if="dia!=0">Your value es {{ dia }}</p>
+    <p v-else>Numero es 0</p>
+    <p v-show="check">How you get here</p>
+    <img :src="imagen1" alt=""/>
+    <img :src="image2" alt=""/>
+</div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { ref,computed } from 'vue';
+import image2 from "./assets/image.png"
+
+const imagen1 = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png";
+const dia = ref(0);
+const plus = () => {
+    dia.value++;
+    if(dia.value==10){
+        console.log(dia.value)
+    }
+};
+const menos = () => {
+    dia.value--;
+
+    if(dia.value<0){
+        console.log("This calue es menor de 0 ")
+
+    }
+    
+};
+
+const check=computed(()=>{
+    return dia.value===0;
+})
+</script>
+
+<style lang="scss"></style>
